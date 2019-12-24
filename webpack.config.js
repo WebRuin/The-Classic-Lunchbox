020b6b7065d258ea84db9bb50a35809db7b7b870
Tim Smith <tim.smith.hdg@gmail.com>
2019-12-24 08:15:52 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./public/app.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  }
+};
