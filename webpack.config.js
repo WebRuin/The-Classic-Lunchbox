25d5a3e402f6a96e44e8541994fc57c0b3310844
Tim Smith <tim.smith.hdg@gmail.com>
2019-12-25 13:46:51 -0800

    Webpack Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,82 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+const SRC_DIR = __dirname + "/src";
+const DIST_DIR = __dirname + "/dist";
+
+let isDevelopment = devMode ? true : false;
+
+console.log(isDevelopment);
+
 module.exports = {
-  entry: "./public/app.js",
-  output: {
-    path: __dirname + "/public/dist",
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        loaders: ["style-loader", "css-loader"]
-      }
-    ]
-  },
+  entry: "./app.js",
   watch: true,
   watchOptions: {
     ignored: /node_modules/
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        enforce: "pre",
+        exclude: /node_modules/,
+        loaders: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            // importLoaders: 1,
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]"
+              },
+              sourceMap: true
+            }
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(html)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "html-loader",
+          options: { minimize: true }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: SRC_DIR + "/index.html",
+      filename: DIST_DIR + "/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    })
+  ],
+  devServer: {
+    contentBase: DIST_DIR,
+    hot: true,
+    port: 4444
   }
 };
