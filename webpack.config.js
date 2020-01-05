c0c93fdecfdc3aafa7560904c363144bfad8e8ab
Tim Smith <tim.smith.hdg@gmail.com>
2020-01-04 23:04:21 -0800

    soooo much!

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,251 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const minJSON = require('jsonminify');
 
-const devMode = process.env.NODE_ENV !== "production";
-const SRC_DIR = __dirname + "/src";
-const DIST_DIR = __dirname + "/dist";
+const plugins = {
+  progress: require('webpackbar'),
+  clean: (() => {
+    const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+    return CleanWebpackPlugin;
+  })(),
+  extractCSS: require('mini-css-extract-plugin'),
+  sync: require('browser-sync-webpack-plugin'),
+  html: require('html-webpack-plugin'),
+  copy: require('copy-webpack-plugin'),
+  sri: require('webpack-subresource-integrity')
+};
 
-// let isDevelopment = devMode ? true : false;
+module.exports = (env = {}, argv) => {
+  const isProduction = argv.mode === 'production';
 
-module.exports = {
-  entry: {
-    app: path.resolve(__dirname, "app.js"),
-    index: path.resolve(SRC_DIR, "index.html")
-  },
-  watch: true,
-  watchOptions: {
-    ignored: /node_modules/
-  },
-  output: {
-    publicPath: "/",
-    path: __dirname + "/dist",
-    filename: "./src/[name].bundle.js"
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        enforce: "pre",
-        exclude: /node_modules/,
-        loaders: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true
+  let config = {
+    context: path.resolve(__dirname, 'src'),
+
+    entry: {
+      // vendor: ['./styles/vendor.scss', './scripts/vendor.js'],
+      app: ['./scss/style.scss', './js/main.js']
+    },
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '',
+      filename: 'scripts/[name].js',
+      crossOriginLoading: 'anonymous'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.((s[ac]|c)ss)$/,
+          use: [
+            {
+              loader: plugins.extractCSS.loader,
+              options: {
+                publicPath: '../' // use relative path for everything in CSS
+              }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProduction
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                sourceMap: !isProduction,
+                plugins: () => [
+                  require('autoprefixer')(),
+                  ...(isProduction
+                    ? [
+                        require('cssnano')({
+                          preset: [
+                            'default',
+                            {
+                              minifySelectors: false
+                            }
+                          ]
+                        })
+                      ]
+                    : [])
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                implementation: require('sass'),
+                sassOptions: {
+                  fiber: require('fibers'),
+                  outputStyle: 'expanded',
+                  sourceMap: !isProduction
+                }
+              }
             }
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          exclude: /fonts/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]'
+                // publicPath: '..' // use relative path
+              }
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                disable: !isProduction,
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65
+                },
+                optipng: {
+                  enabled: false
+                },
+                pngquant: {
+                  quality: [0.65, 0.9],
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false
+                },
+                webp: {
+                  quality: 75
+                }
+              }
+            }
+          ]
+        },
+        {
+          test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          exclude: /images/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+                // publicPath: '../fonts/' // use relative path
+              }
+            }
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              removeComments: true,
+              collapseWhitespace: true,
+              removeScriptTypeAttributes: true,
+              removeStyleTypeAttributes: true
+            }
+          }
+        }
+      ]
+    },
+
+    devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      port: 8080,
+      overlay: {
+        warnings: true,
+        errors: true
+      },
+      quiet: true
+    },
+
+    plugins: (() => {
+      let common = [
+        new plugins.extractCSS({
+          filename: 'styles/[name].css'
+        }),
+        new plugins.html({
+          template: 'index.html',
+          filename: 'index.html',
+          minify: {
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+          }
+        }),
+        new plugins.html({
+          template: './subPages/menu.html',
+          filename: 'menu.html',
+          minify: {
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+          }
+        }),
+        new plugins.progress({
+          color: '#b3129d'
+        })
+      ];
+
+      const production = [
+        new plugins.clean(),
+        new plugins.copy([
+          {
+            from: 'data/**/*.json',
+            transform: content => {
+              return minJSON(content.toString());
+            }
+          }
+        ]),
+        new plugins.sri({
+          hashFuncNames: ['sha384'],
+          enabled: true
+        })
+      ];
+
+      const development = [
+        new plugins.sync(
+          {
+            host: 'localhost',
+            port: 9090,
+            proxy: 'http://localhost:8080/'
           },
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.js$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(html)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "html-loader",
-          options: { minimize: true }
-        }
+          {
+            reload: false
+          }
+        )
+      ];
+
+      return isProduction
+        ? common.concat(production)
+        : common.concat(development);
+    })(),
+
+    devtool: (() => {
+      return isProduction
+        ? '' // 'hidden-source-map'
+        : 'source-map';
+    })(),
+
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      alias: {
+        '~': path.resolve(__dirname, 'src/scripts/')
       }
-    ]
-  },
-  resolve: {
-    extensions: ["*", ".js"]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      inject: "head",
-      filename: path.resolve(DIST_DIR, "index.html"),
-      template: path.resolve(SRC_DIR, "index.html")
-    }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? "[name].css" : "[name].[hash].css",
-      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
-    })
-  ],
-  devServer: {
-    contentBase: DIST_DIR,
-    hot: true,
-    port: 4400
-  }
+    },
+
+    stats: 'errors-only'
+  };
+
+  return config;
 };
